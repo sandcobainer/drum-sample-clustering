d8a08447c43d78375316bdb2759440a9cbc85957
sandeep <sandeep.cr007@gmail.com>
2019-10-19 14:32:30 -0400

    compute features (10) complete

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import os
+import sys
+sys.path.append('/usr/local/lib/python3/dist-packages/')
 
-from essentia import *
-from essentia.standard import *
+import essentia
+import essentia.standard as es
+
 
 def isMatch(name, patterns):
     if not patterns:
@@ -32,7 +35,6 @@ def pool_to_array(pool, include_descs=None, ignore_descs=None):
     i=0
     # append everything to dict result
     for d in descs:
-        print(i)
         value = pool[d]
         print(value)
         if type(value) is np.ndarray:
@@ -42,45 +44,82 @@ def pool_to_array(pool, include_descs=None, ignore_descs=None):
         #add_to_dict(result, keys, value)
     return result
 
-def run(complete_path):
+def compute_features(complete_path):
+    result = np.array([])
     # for loop over files
     for file in os.listdir(complete_path):
         if file.endswith(".wav"):
             print(file)
             # load our audio into an array
-            audio = MonoLoader(filename=complete_path + file, sampleRate=44100)()
+            audio = es.MonoLoader(filename=complete_path + file, sampleRate=44100)()
 
             # create the pool and the necessary algorithms
-            pool = Pool()
-            w = Windowing()
-            spec = Spectrum()
-            centroid = Centroid(range=22050)
-            rolloff = R
+            pool = essentia.Pool()
+            window = es.Windowing()
+            spectrum = es.Spectrum()
+            centroid = es.Centroid(range=22050)
+            rolloff = es.RollOff()
+            crest = es.Crest()
+            speak = es.StrongPeak()
+            rmse = es.RMS()
+            mfcc = es.MFCC()
+            flux = es.Flux()
+            barkbands = es.BarkBands( sampleRate = 44100)
+            zerocrossingrate = es.ZeroCrossingRate()
+            
+            # centralmoments = es.SpectralCentralMoments()
+            # distributionshape = es.DistributionShape()
 
             # compute the centroid for all frames in our audio and add it to the pool
-            for frame in FrameGenerator(audio, frameSize = 1024, hopSize = 512):
-                c = centroid(spec(w(frame)))
+            for frame in es.FrameGenerator(audio, frameSize = 1024, hopSize = 512):
+                frame_windowed = window(frame)
+                frame_spectrum = spectrum(frame_windowed)
+                
+                c = centroid(frame_spectrum)
                 pool.add('spectral.centroid', c)
 
+                cr = crest(frame_spectrum)
+                pool.add('spectral crest', cr)
+
+                r = rolloff(frame_spectrum)
+                pool.add('spectral rolloff', r)
+
+                sp = speak(frame_spectrum)
+                pool.add('strong peak', sp)
+
+                rms = rmse(frame_spectrum)
+                pool.add('RMS', rms)
+
+                # (frame_melbands, frame_mfcc) = mfcc(frame_spectrum)
+                # pool.add('frame_MFCC', frame_mfcc)
+
+                fl = flux(frame_spectrum)
+                pool.add('spectral flux', fl)
+
+                # bbands = barkbands(frame_spectrum)
+                # pool.add('bark bands', bbands)
+
+                zcr = zerocrossingrate(frame_spectrum)
+                pool.add('zero crossing rate', zcr)
+
+                # frame_centralmoments = centralmoments(power_spectrum)
+                # (frame_spread, frame_skewness, frame_kurtosis) = distributionshape(frame_centralmoments)
+                # pool.add('spectral_kurtosis', frame_kurtosis)
+                # pool.add('spectral_spread', frame_spread)
+                # pool.add('spectral_skewness', frame_skewness)
+
             # aggregate the results (find mean if needed)
-            aggrpool = PoolAggregator(defaultStats = [ 'mean'])(pool) #,'stdev' ])(pool)
-
+            aggrpool = es.PoolAggregator(defaultStats = [ 'mean'])(pool) #,'stdev' ])(pool)
             # write aggrpool to dict
-            result = pool_to_array(aggrpool)w
-            print(result)
+            aggr_np = pool_to_array(aggrpool)
 
-            # loader = essentia.standard.MonoLoader(complete_path + file)
-            # audio = loader()
-            # w = Windowing(type = 'hann')
-            # for frame in FrameGenerator(audio, frameSize=1024, hopSize=512, startFromZero=True):
-
-            # features, features_frames = es.MusicExtractor(lowlevelStats=['mean', 'stdev'],
-            #                                   rhythmStats=['mean', 'stdev'],
-            #                                   tonalStats=['mean', 'stdev'])(complete_path + file)
-            # print(sorted(features['lowlevel'])
+            result = np.append(result, aggr_np)
+            
+    return result
 
 # See all feature names in the pool in a sorted order
 # print(sorted(features.descriptorNames()))
 
 if __name__ == '__main__':
-    run('./samples/')
+    features = compute_features('./samples/')
+    print(features.shape)
